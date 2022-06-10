7adb07281966112ca67fedd8ab3d3198ef33154a
Max Girteit <icufishmg@gmail.com>
2022-06-10 16:38:56 -0600

    icons added to EventItems

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,20 +1,25 @@
-import Link from 'next/link'
-import DateIcon from '../icons/icons/date-icon'
+import AddressIcon from '../icons/address-icon';
+import ArrowRightIcon from '../icons/arrow-right-icon';
+import DateIcon from '../icons/date-icon';
 
 import Button from '../ui/button';
 
-import classes from './EventItem.module.css'
+import classes from './EventItem.module.css';
 
 const EventItem = (props) => {
-    const { title, image, date, location, id } = props;
+  const { title, image, date, location, id } = props;
 
-    console.log(props)
+  console.log(props);
 
-    const readableDate = new Date(date).toLocaleDateString('en-US', {day: 'numeric', month: 'long', year: 'numeric'});
+  const readableDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
 
-    const addressFormatted = location.replace(', ', '\n')
+  const addressFormatted = location.replace(', ', '\n');
 
-    const directoryLink = `/events/${id}`;
+  const directoryLink = `/events/${id}`;
 
   return (
     <li className={classes.item}>
@@ -24,12 +29,20 @@ const EventItem = (props) => {
           <h2>{title}</h2>
           <div className={classes.date}>
             <DateIcon />
-              <time>{readableDate}</time>
+            <time>{readableDate}</time>
           </div>
-          <div className={classes.address}><address>{addressFormatted}</address></div>
+          <div className={classes.address}>
+            <AddressIcon />
+            <address>{addressFormatted}</address>
+          </div>
         </div>
         <div className={classes.actions}>
-            <Button link={directoryLink}>Explore Event</Button>
+          <Button link={directoryLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
