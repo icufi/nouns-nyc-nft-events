bd83ee7dbe9abe348175d7d4a501055ebf750c52
Max Girteit <icufishmg@gmail.com>
2022-06-11 13:57:07 -0600

    displays event by id

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,5 +1,35 @@
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+
+import { getEventById } from '../../nouns-NYC-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
+
 const EventDetails = () => {
-  return <div>EventDetails</div>;
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        alt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
+  );
 };
 
 export default EventDetails;
