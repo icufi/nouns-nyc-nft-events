3f8e6b1fff2fe44452d5027a8306c4783b125397
Max Girteit <icufishmg@gmail.com>
2022-06-13 17:50:44 -0600

    filtered events by dynamic slug address

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+
+import { getFilteredEvents } from '../../nouns-NYC-data';
+import EventList from '../../components/events/EventList';
 
 const FilteredEvents = () => {
-  return (
-    <div>FilteredEvents</div>
-  )
-}
+  const router = useRouter();
+  const filterData = router.query.slug;
 
-export default FilteredEvents
+  if (!filterData) {
+    return <p className='center'>Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth > 12 ||
+    numMonth < 1
+  ) {
+    return <p>Invalid Filter. Please adjust merch values.</p>
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No Noun events found.</p>
+  }
+
+
+  return (
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
+  );
+};
+
+export default FilteredEvents;
