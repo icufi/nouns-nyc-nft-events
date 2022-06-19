1135073c5fe5b54e25a284dab2a2ba5173c5e538
Max Girteit <icufishmg@gmail.com>
2022-06-18 19:58:44 -0600

    removed console.logs

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import EventList from '../components/events/EventList';
 import { getFeaturedEvents } from '../helpers/api-utils';
 
 const HomePage = (props) => {
-  console.log(props)
+
   return (
     <div>
       <EventList items={props.events} />
@@ -12,7 +12,7 @@ const HomePage = (props) => {
 
 export async function getStaticProps() {
   const featuredEvents = await getFeaturedEvents();
-  console.log(featuredEvents);
+  
   return {
     props: {
       events: featuredEvents,
