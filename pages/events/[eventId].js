f1e13b62b687dc455df9ad8f4b68661eb7117931
Max Girteit <icufishmg@gmail.com>
2022-06-18 20:18:35 -0600

    static generation added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,17 +1,13 @@
 import { Fragment } from 'react';
-import { useRouter } from 'next/router';
 
-import { getEventById } from '../../nouns-NYC-data';
+import { getEventById, getAllEvents } from '../../helpers/api-utils';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-const EventDetails = () => {
-  const router = useRouter();
-
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+const EventDetails = (props) => {
+  const event = props.selectedEvent;
 
   if (!event) {
     return (
@@ -36,5 +32,27 @@ const EventDetails = () => {
     </Fragment>
   );
 };
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+  console.log(paths)
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
 
 export default EventDetails;
